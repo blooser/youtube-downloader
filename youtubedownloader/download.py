40a3b01541988dcab3c0a7543041c76f180e0370
blooser <Blooser@protonmail.com>
2020-04-13 02:23:12 +0200

    Reorganize
    To make it more readable
    
    Signed-off-by: blooser <Blooser@protonmail.com>

--- youtubedownloader/download.py
+++ youtubedownloader/download.py
@@ -11,243 +11,11 @@ import youtube_dl
 from .logger import create_logger
 from .settings import Settings
 
-class DownloadProgress(QObject):
-    changed = Signal()
-
-    def __init__(self):
-        super(DownloadProgress, self).__init__(None)
-        self.status = str("queued")
-        self.downloaded_bytes = str("0")
-        self.total_bytes = str("0")
-        self.estimated_time = str("00:00")
-        self.speed = str("0 MiB/s")
-        self.filename = str("Unknown")
-
-    @Property(str, notify=changed)
-    def downloadStatus(self):
-        return self.status
-
-    @Property(int, notify=changed)
-    def downloadedBytes(self):
-        return int(self.downloaded_bytes)
-
-    @Property(int, notify=changed)
-    def totalBytes(self):
-        return int(self.total_bytes)
-
-    @Property(str, notify=changed)
-    def estimatedTime(self):
-        return self.estimated_time
-
-    @Property(str, notify=changed)
-    def downloadSpeed(self):
-        return self.speed
-
-    def update(self, data):
-        if "status" in data:
-            self.status = data["status"]
-
-        if "downloaded_bytes" in data:
-            self.downloaded_bytes = data["downloaded_bytes"]
-
-        if "total_bytes" in data:
-            self.total_bytes = data["total_bytes"]
-
-        if "_eta_str" in data:
-            self.estimated_time = data["_eta_str"]
-
-        if "_speed_str" in data:
-            self.speed = data["_speed_str"]
-
-        if "filename" in data:
-            self.filename = data["filename"]
-
-        self.changed.emit() # TODO: Maybe separated signal for each property?
-
-    @staticmethod
-    def pack(download_progress):
-        return {
-            "status": download_progress.status,
-            "downloaded_bytes": download_progress.downloaded_bytes,
-            "total_bytes": download_progress.total_bytes,
-            "estimated_time": download_progress.estimated_time,
-            "speed": download_progress.speed,
-            "filename": download_progress.filename
-        }
-
-    @staticmethod
-    def unpack(data):
-        download_progress = DownloadProgress()
-        download_progress.status = data["status"]
-        download_progress.downloaded_bytes = data["downloaded_bytes"]
-        download_progress.total_bytes = data["total_bytes"]
-        download_progress.estimated_time = data["estimated_time"]
-        download_progress.speed = data["speed"]
-        download_progress.filename = data["filename"]
-        return download_progress
-
-
-class DownloadOptions(QObject):
-    OUTPUT_FILE = "%(title)s.%(ext)s"
-
-    MP3_TEMPLATE = {
-        "postprocessors": [{
-               "key": 'FFmpegExtractAudio',
-               "preferredcodec": 'mp3',
-               "preferredquality": "192",
-            }]
-        }
-
-    MP4_TEMPLATE = {
-       "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"
-    }
-
-    WEBM_TEMPLATE = {}
-
-    changed = Signal(str)
-
-    def __init__(self, options):
-        super(DownloadOptions, self).__init__(None)
-        self.file_format = options["file_format"]
-        self.output_path = options["output_path"]
-        self.ydl_opts = {
-            "format": "bestaudio/best"
-        }
-
-        self.post_process_file_size = int(options["post_process_file_size"]) if "post_process_file_size" in options else 0
-
-    def __eq__(self, other):
-        return self.file_format == other.file_format and self.output_path == other.output_path
-
-    @Property(str, notify=changed)
-    def fileFormat(self):
-        return self.file_format
-
-    @Property(str, notify=changed)
-    def outputPath(self):
-        return self.output_path
-
-    def calc_post_process_file_size(self, duration):
-        self.post_process_file_size = (((192 * duration)/8) * 1000)
-
-    def to_ydl_opts(self):
-        template = self.ydl_opts
-        template.update(self.output_template())
-        template.update(self.post_processors())
-        return template
-
-    def need_post_process(self):
-        return self.file_format in ["mp3"]
-
-    def output_template(self):
-        return {
-            "outtmpl": self.output_path + "/" + DownloadOptions.OUTPUT_FILE
-        }
-
-    def post_processors(self):
-        if "mp3" in self.file_format:
-            return DownloadOptions.MP3_TEMPLATE
-
-        elif "mp4" in self.file_format:
-            return DownloadOptions.MP4_TEMPLATE
-
-        elif "webm" in self.file_format:
-            return DownloadOptions.WEBM_TEMPLATE
-
-        return {}
-
-    @staticmethod
-    def pack(download_options):
-        return {
-            "file_format": download_options.file_format,
-            "output_path": download_options.output_path,
-            "post_process_file_size": download_options.post_process_file_size
-        }
-
-    @staticmethod
-    def unpack(data):
-        download_options = DownloadOptions(data)
-        return download_options
-
 
 class DownloadCommunication(QObject):
     updated = Signal(str)
     progress = Signal(dict)
     start = Signal()
-
-
-class Download(QObject):
-    def __init__(self, data):
-        super(Download, self).__init__(None)
-        self.id = data["id"]
-        self.url = data["url"]
-        self.title = data["title"]
-        self.uploader = data["uploader"]
-        self.thumbnail = data["thumbnail"]
-        self.duration = data["duration"]
-        self.download_options = DownloadOptions.unpack(data["download_options"])
-
-        self.progress = DownloadProgress()
-        self.communication = DownloadCommunication()
-
-    @Slot(dict)
-    def update(self, data):
-        self.progress.update(data)
-        self.communication.updated.emit(str(self.id)) # PySide2's Signal doesn't handle such big number, to avoid Overflowing we use string
-
-    def __eq__(self, other):
-        return self.id == other.id
-
-    @staticmethod
-    def pack(download):
-        return {
-            "id": download.id,
-            "url": download.url,
-            "title": download.title,
-            "uploader": download.uploader,
-            "thumbnail": download.thumbnail,
-            "duration": download.duration,
-            "download_options": DownloadOptions.pack(download.download_options),
-            "progress": DownloadProgress.pack(download.progress)
-        }
-
-    @staticmethod
-    def unpack(data):
-        download = Download(data)
-        download.progress = DownloadProgress.unpack(data["progress"])
-        return download
-
-    @classmethod
-    def fromPreDownload(cls, predownload):
-        return cls(PreDownload.pack(predownload))
-
-class DownloadPostProcess(QObject):
-    bytes_processed = Signal(int)
-    started = Signal()
-    finished = Signal()
-
-    def __init__(self):
-        super(DownloadPostProcess, self).__init__(None)
-
-        self.file_watcher = QFileSystemWatcher()
-        self.file_watcher.fileChanged.connect(self.read_bytes)
-        self.total_bytes = None
-
-        self.logger = create_logger(__name__)
-
-    def track(self, url):
-        track_success = self.file_watcher.addPath(url)
-        self.logger.info("Track {file} success={success}".format(file=url, success=track_success))
-        self.started.emit()
-
-    @Slot(str)
-    def read_bytes(self, path):
-        bytes = QFileInfo(path).size()
-        self.bytes_processed.emit(bytes)
-        self.logger.debug("Read {bytes} bytes".format(bytes=bytes))
-
-        if bytes >= self.total_bytes:
-            self.finished.emit()
 
 
 class PreDownloadTask(QRunnable):
@@ -261,43 +29,6 @@ class PreDownloadTask(QRunnable):
             info = ydl.extract_info(self.url, download=False)
 
         self.communication.progress.emit(info)
-
-
-class DownloadTask(QRunnable):
-    def __init__(self, url, options):
-        super(DownloadTask, self).__init__(None)
-        self.url = url
-        self.options = options
-        self.ydl_opts = self.options.to_ydl_opts()
-
-        self.ydl_opts["progress_hooks"] = [self.process]
-
-        self.communication = DownloadCommunication()
-
-        self.download_post_process = DownloadPostProcess()
-        self.download_post_process.total_bytes = self.options.post_process_file_size
-        self.post_process_file = str()
-        self.post_process_timer = QTimer()
-        self.post_process_timer.setInterval(500)
-        self.post_process_timer.setSingleShot(True)
-        self.post_process_timer.timeout.connect(lambda: self.download_post_process.track(self.post_process_file))
-
-        self.communication.start.connect(self.post_process_timer.start)
-        self.download_post_process.bytes_processed.connect(lambda bytes: self.communication.progress.emit({"downloaded_bytes": bytes}), Qt.QueuedConnection)
-        self.download_post_process.finished.connect(lambda: self.communication.progress.emit({"status": "finished"}), Qt.QueuedConnection)
-        self.download_post_process.started.connect(lambda: self.communication.progress.emit({"status": "Converting to {0}".format(self.options.file_format),
-                                                                                             "total_bytes": self.options.post_process_file_size}), Qt.QueuedConnection)
-
-    def process(self, data):
-        self.communication.progress.emit(data)
-
-        if self.options.need_post_process() and data["status"] == "finished":
-            self.post_process_file = os.path.join(self.options.output_path, "{file}.{ext}".format(file=pathlib.PurePath(data["filename"]).stem, ext=self.options.file_format))
-            self.communication.start.emit()
-
-    def run(self):
-        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
-            ydl.download([self.url])
 
 
 class PreDownload(object):
@@ -461,6 +192,276 @@ class PreDownloadModel(QAbstractListModel):
             return predownload.download_options
 
         return None
+
+
+class DownloadProgress(QObject):
+    changed = Signal()
+
+    def __init__(self):
+        super(DownloadProgress, self).__init__(None)
+        self.status = str("queued") # NOTE: Think about better status notification
+        self.downloaded_bytes = str("0")
+        self.total_bytes = str("0")
+        self.estimated_time = str("00:00")
+        self.speed = str("0 MiB/s")
+        self.filename = str("Unknown")
+
+    @Property(str, notify=changed)
+    def downloadStatus(self):
+        return self.status
+
+    @Property(int, notify=changed)
+    def downloadedBytes(self):
+        return int(self.downloaded_bytes)
+
+    @Property(int, notify=changed)
+    def totalBytes(self):
+        return int(self.total_bytes)
+
+    @Property(str, notify=changed)
+    def estimatedTime(self):
+        return self.estimated_time
+
+    @Property(str, notify=changed)
+    def downloadSpeed(self):
+        return self.speed
+
+    def update(self, data):
+        if "status" in data:
+            self.status = data["status"]
+
+        if "downloaded_bytes" in data:
+            self.downloaded_bytes = data["downloaded_bytes"]
+
+        if "total_bytes" in data:
+            self.total_bytes = data["total_bytes"]
+
+        if "_eta_str" in data:
+            self.estimated_time = data["_eta_str"]
+
+        if "_speed_str" in data:
+            self.speed = data["_speed_str"]
+
+        if "filename" in data:
+            self.filename = data["filename"]
+
+        self.changed.emit() # TODO: Maybe separated signal for each property?
+
+    @staticmethod
+    def pack(download_progress):
+        return {
+            "status": download_progress.status,
+            "downloaded_bytes": download_progress.downloaded_bytes,
+            "total_bytes": download_progress.total_bytes,
+            "estimated_time": download_progress.estimated_time,
+            "speed": download_progress.speed,
+            "filename": download_progress.filename
+        }
+
+    @staticmethod
+    def unpack(data):
+        download_progress = DownloadProgress()
+        download_progress.status = data["status"]
+        download_progress.downloaded_bytes = data["downloaded_bytes"]
+        download_progress.total_bytes = data["total_bytes"]
+        download_progress.estimated_time = data["estimated_time"]
+        download_progress.speed = data["speed"]
+        download_progress.filename = data["filename"]
+        return download_progress
+
+
+class DownloadOptions(QObject):
+    OUTPUT_FILE = "%(title)s.%(ext)s"
+
+    MP3_TEMPLATE = {
+        "postprocessors": [{
+               "key": 'FFmpegExtractAudio',
+               "preferredcodec": 'mp3',
+               "preferredquality": "192",
+            }]
+        }
+
+    MP4_TEMPLATE = {
+       "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"
+    }
+
+    WEBM_TEMPLATE = {}
+
+    changed = Signal(str)
+
+    def __init__(self, options):
+        super(DownloadOptions, self).__init__(None)
+        self.file_format = options["file_format"]
+        self.output_path = options["output_path"]
+        self.ydl_opts = {
+            "format": "bestaudio/best"
+        }
+
+        self.post_process_file_size = int(options["post_process_file_size"]) if "post_process_file_size" in options else 0
+
+    def __eq__(self, other):
+        return self.file_format == other.file_format and self.output_path == other.output_path
+
+    @Property(str, notify=changed)
+    def fileFormat(self):
+        return self.file_format
+
+    @Property(str, notify=changed)
+    def outputPath(self):
+        return self.output_path
+
+    def calc_post_process_file_size(self, duration):
+        self.post_process_file_size = (((192 * duration)/8) * 1000)
+
+    def to_ydl_opts(self):
+        template = self.ydl_opts
+        template.update(self.output_template())
+        template.update(self.post_processors())
+        return template
+
+    def need_post_process(self):
+        return self.file_format in ["mp3"] # TODO: Add more file formats
+
+    def output_template(self):
+        return {
+            "outtmpl": self.output_path + "/" + DownloadOptions.OUTPUT_FILE
+        }
+
+    def post_processors(self):
+        if "mp3" in self.file_format:
+            return DownloadOptions.MP3_TEMPLATE
+
+        elif "mp4" in self.file_format:
+            return DownloadOptions.MP4_TEMPLATE
+
+        elif "webm" in self.file_format:
+            return DownloadOptions.WEBM_TEMPLATE
+
+        return {}
+
+    @staticmethod
+    def pack(download_options):
+        return {
+            "file_format": download_options.file_format,
+            "output_path": download_options.output_path,
+            "post_process_file_size": download_options.post_process_file_size
+        }
+
+    @staticmethod
+    def unpack(data):
+        download_options = DownloadOptions(data)
+        return download_options
+
+
+class DownloadTask(QRunnable):
+    def __init__(self, url, options):
+        super(DownloadTask, self).__init__(None)
+        self.url = url
+        self.options = options
+        self.ydl_opts = self.options.to_ydl_opts()
+
+        self.ydl_opts["progress_hooks"] = [self.process]
+
+        self.communication = DownloadCommunication()
+
+        self.download_post_process = DownloadPostProcess()
+        self.download_post_process.total_bytes = self.options.post_process_file_size
+        self.post_process_file = str()
+        self.post_process_timer = QTimer()
+        self.post_process_timer.setInterval(500)
+        self.post_process_timer.setSingleShot(True)
+        self.post_process_timer.timeout.connect(lambda: self.download_post_process.track(self.post_process_file))
+
+        self.communication.start.connect(self.post_process_timer.start)
+        self.download_post_process.bytes_processed.connect(lambda bytes: self.communication.progress.emit({"downloaded_bytes": bytes}), Qt.QueuedConnection)
+        self.download_post_process.finished.connect(lambda: self.communication.progress.emit({"status": "finished"}), Qt.QueuedConnection)
+        self.download_post_process.started.connect(lambda: self.communication.progress.emit({"status": "Converting to {0}".format(self.options.file_format),
+                                                                                             "total_bytes": self.options.post_process_file_size}), Qt.QueuedConnection)
+    def process(self, data):
+        self.communication.progress.emit(data)
+
+        if self.options.need_post_process() and data["status"] == "finished":
+            self.post_process_file = os.path.join(self.options.output_path, "{file}.{ext}".format(file=pathlib.PurePath(data["filename"]).stem, ext=self.options.file_format))
+            self.communication.start.emit()
+
+    def run(self):
+        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+            ydl.download([self.url])
+
+
+class Download(QObject):
+    def __init__(self, data):
+        super(Download, self).__init__(None)
+        self.id = data["id"]
+        self.url = data["url"]
+        self.title = data["title"]
+        self.uploader = data["uploader"]
+        self.thumbnail = data["thumbnail"]
+        self.duration = data["duration"]
+        self.download_options = DownloadOptions.unpack(data["download_options"])
+
+        self.progress = DownloadProgress()
+        self.communication = DownloadCommunication()
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    @Slot(dict)
+    def update(self, data):
+        self.progress.update(data)
+        self.communication.updated.emit(str(self.id)) # PySide2's Signal doesn't handle such big number, to avoid Overflowing we use string
+
+    @staticmethod
+    def pack(download):
+        return {
+            "id": download.id,
+            "url": download.url,
+            "title": download.title,
+            "uploader": download.uploader,
+            "thumbnail": download.thumbnail,
+            "duration": download.duration,
+            "download_options": DownloadOptions.pack(download.download_options),
+            "progress": DownloadProgress.pack(download.progress)
+        }
+
+    @staticmethod
+    def unpack(data):
+        download = Download(data)
+        download.progress = DownloadProgress.unpack(data["progress"])
+        return download
+
+    @classmethod
+    def fromPreDownload(cls, predownload):
+        return cls(PreDownload.pack(predownload))
+
+
+class DownloadPostProcess(QObject):
+    bytes_processed = Signal(int)
+    started = Signal()
+    finished = Signal()
+
+    def __init__(self):
+        super(DownloadPostProcess, self).__init__(None)
+
+        self.file_watcher = QFileSystemWatcher()
+        self.file_watcher.fileChanged.connect(self.read_bytes)
+        self.total_bytes = None
+
+        self.logger = create_logger(__name__)
+
+    def track(self, url):
+        track_success = self.file_watcher.addPath(url)
+        self.logger.info("Track {file} success={success}".format(file=url, success=track_success))
+        self.started.emit()
+
+    @Slot(str)
+    def read_bytes(self, path):
+        bytes = QFileInfo(path).size()
+        self.bytes_processed.emit(bytes)
+        self.logger.debug("Read {bytes} bytes".format(bytes=bytes))
+
+        if bytes >= self.total_bytes:
+            self.finished.emit()
 
 
 class DownloadModel(QAbstractListModel):
