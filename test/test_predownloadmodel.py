bffb4f72e024324451b73656d59e2877676df43a
blooser <Blooser@protonmail.com>
2020-04-13 17:07:34 +0200

    Update unit test
    Signed-off-by: blooser <Blooser@protonmail.com>

--- test/test_predownloadmodel.py
+++ test/test_predownloadmodel.py
@@ -12,7 +12,7 @@ class PreDownloadModelTest(unittest.TestCase):
     def setUp(self):
         self.yt_url = "https://www.youtube.com/watch?v=3L65PG_eZFg"
         
-        self.config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "predownloadmodelconfig")
+        self.config_path = "predownloadmodelconfig"
         
         self.options = {
                 "file_format": "mp3",
@@ -25,10 +25,7 @@ class PreDownloadModelTest(unittest.TestCase):
             os.remove(self.config_path)
 
 
-    def test_preDownloadModelSavesAndLoadData(self):
-            if os.path.isfile(self.config_path):
-                os.remove(self.config_path)
-        
+    def test_preDownloadModelSavesAndLoadData(self):        
             predownload = PreDownload(self.yt_url, self.options)
             predownload_model = PreDownloadModel(self.config_path)
             predownload_model.add_predownload(predownload)
@@ -40,7 +37,7 @@ class PreDownloadModelTest(unittest.TestCase):
             
             second_predownload = second_predownload_model.predownloads[0]
             self.assertEqual(second_predownload.download_options, DownloadOptions(self.options))
-            self.assertEqual(second_predownload.url, self.yt_url )
+            self.assertEqual(second_predownload.url, self.yt_url)
 
 
 if __name__ == "__main__":
