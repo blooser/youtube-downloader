90e61bdb9446a64887bf3aef7bbb6d120e1614f8
blooser <Blooser@protonmail.com>
2020-05-07 14:06:53 +0200

    Update test for new settings variable
    Signed-off-by: blooser <Blooser@protonmail.com>

--- test/test_settings.py
+++ test/test_settings.py
@@ -6,6 +6,7 @@ import pickle
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from PySide2.QtCore import QRect
 from youtubedownloader import Settings
 
 
@@ -23,9 +24,11 @@ class SettingsTest(unittest.TestCase):
         settings1.input_link = "link"
         settings1.file_format = "mp4"
         settings1.output_path = "/foo/bar/path1"
+        settings1.window_rect = QRect(100, 100, 750, 750)
         del settings1
         
         settings2 = Settings(self.config_path)
         self.assertEqual(settings2.input_link, "link")
         self.assertEqual(settings2.file_format, "mp4")
         self.assertEqual(settings2.output_path, "/foo/bar/path1")
+        self.assertEqual(settings2.window_rect, QRect(100, 100, 750, 750))
