169f54b68eaaf1339f8c0addf27151241675d0ec
blooser <Blooser@protonmail.com>
2020-05-13 23:07:41 +0200

    Version 0.2.1
    Improve regex to match only links which provide to video
    
    Signed-off-by: blooser <Blooser@protonmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup (
     name="youtube-downloader",
-    version="0.2.0",
+    version="0.2.1",
     license="GPLv3",
     author="blooser",
     url="https://www.github.com/blooser/youtube-downloader",
