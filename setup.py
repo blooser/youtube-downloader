78b1418e613f4b8596a9088da45a7e219099e3b9
blooser <Blooser@protonmail.com>
2020-05-13 21:16:30 +0200

    Version 0.2.0
    Signed-off-by: blooser <Blooser@protonmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup (
     name="youtube-downloader",
-    version="0.1.0",
+    version="0.2.0",
     license="GPLv3",
     author="blooser",
     url="https://www.github.com/blooser/youtube-downloader",
