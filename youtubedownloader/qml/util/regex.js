d5f9425b6dc29132921025993d025daa06a4fb97
blooser <Blooser@protonmail.com>
2020-05-13 21:54:41 +0200

    Update regex
    Signed-off-by: blooser <Blooser@protonmail.com>

--- youtubedownloader/qml/util/regex.js
+++ youtubedownloader/qml/util/regex.js
@@ -1,5 +1,5 @@
 ﻿const URL_REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-const YOUTUBE_LINK_REGEX = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/gm
+const YOUTUBE_LINK_REGEX = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/gm
 
 function isUrl(url){
     let matches = url.match(URL_REGEX)
