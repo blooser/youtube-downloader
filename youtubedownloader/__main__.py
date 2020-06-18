a72a7e4ef2055335a99c1cd998259b46d2e13bc7
blooser <Blooser@protonmail.com>
2020-06-18 17:12:24 +0200

    Version 0.3.0
    Signed-off-by: blooser <Blooser@protonmail.com>

--- youtubedownloader/__main__.py
+++ youtubedownloader/__main__.py
@@ -21,7 +21,7 @@ def main():
 
     app = QApplication(sys.argv)
     app.setApplicationName("youtube downloader")
-    app.setApplicationVersion("0.1.0")
+    app.setApplicationVersion("0.3.0")
     app.setOrganizationName("blooser")
     app.setWindowIcon(QIcon(Resources.YD_LOGO))
 
