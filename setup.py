9ddb14a086c57318fa31402ec898b9b4931addad
blooser <Blooser@protonmail.com>
2020-06-25 20:45:22 +0200

    Add beautifulsoup4 as requires
    Signed-off-by: blooser <Blooser@protonmail.com>

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup (
     install_requires = [
         "PySide2",
         "lz4",
+        "beautifulsoup4",
         "youtube-dl"
     ],
     keywords = "qt qml pyside2 youtube download youtube-dl",
