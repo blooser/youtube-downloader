a5890b03689dd9bfeeca645aea2cae86958ef885
blooser <Blooser@protonmail.com>
2020-06-28 14:26:21 +0200

    Implement database module
    Control the database
    
    Signed-off-by: blooser <Blooser@protonmail.com>

---
+++ youtubedownloader/database/__init__.py
@@ -0,0 +1,18 @@
+﻿from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Database(object):
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        self.engine = create_engine("sqlite:///{db_path}".format(db_path=self.db_path))
+        self.session = sessionmaker(bind=self.engine, autocommit=True, autoflush=True)()
+
+        self.initialize()
+
+    def initialize(self):
+        from . import models
+        Base.metadata.create_all(bind=self.engine)
+
---
+++ youtubedownloader/database/models.py
@@ -0,0 +1,13 @@
+﻿from sqlalchemy import Column, Integer, String
+
+from . import Base
+
+class History(Base):
+    __tablename__ = "history"
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String(80), unique=True)
+    title = Column(String(80))
+    uploader = Column(String(80))
+    thumbnail = Column(String(80))
+
